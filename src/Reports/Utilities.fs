1bd9f89ca253e2f1a86b18baad01b7b0cd35e8bf
Kurt Mueller <kurt.mueller@osumc.edu>
2021-01-26 14:11:14 -0500

    Create helpers to read from & write to disk

--- src/Reports/Utilities.fs
+++ src/Reports/Utilities.fs
@@ -8,6 +8,15 @@ module Utilities =
         let remove regex string =
             Regex.Replace(string, regex, "")
 
+    module String =
+        open System.IO
+
+        let readFromDisk path = File.ReadAllText(path)
+
+        let writeToDisk path string =
+            File.WriteAllText(path, string)
+
+
     module Xml =
         open System.IO
         open System.Linq
@@ -18,10 +27,11 @@ module Utilities =
             let mutable schemaSet = XmlSchemaSet()
             let schemaInference = XmlSchemaInference()
 
-            member _.AddXml (path: string) =
+            member this.AddXml (path: string) =
                 let xmlReader = XmlReader.Create(path)
 
                 schemaSet <- schemaInference.InferSchema(xmlReader, schemaSet)
+                this
 
             member _.Schemas = Enumerable.Cast<XmlSchema>(schemaSet.Schemas())
 
@@ -32,5 +42,16 @@ module Utilities =
                 this.Schema.Write(stringWriter)
                 stringWriter.ToString()
 
+            member this.SchemaStrings =
+                this.Schemas |> Seq.map (fun schema ->
+                    let stringWriter = new StringWriter()
+                    schema.Write(stringWriter)
+                    stringWriter.ToString()
+                )
+
             member this.SaveSchemaToDisk path =
                 File.WriteAllText(path, this.SchemaString)
+
+        module SchemaGenerator =
+            let addXml xmlPath (schemaGenerator: XmlSchemaGenerator) =
+                schemaGenerator.AddXml xmlPath
