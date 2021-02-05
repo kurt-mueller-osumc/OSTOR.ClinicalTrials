96a64ebf57cbc2408fb9a0fa965199209fd5673a
Kurt Mueller <kurt.mueller@osumc.edu>
2021-02-04 22:24:08 -0500

    Read in patient info from tempus json

--- src/Reports/Tempus.fs
+++ src/Reports/Tempus.fs
@@ -3,9 +3,16 @@ namespace OSTOR.ClinicalTrials.Reports
 module Tempus =
     open Newtonsoft.Json
 
+    type Diagnosis =
+        { DiagnosisName: DiagnosisName
+          DiagnosisDate: DiagnosisDate option }
+    and DiagnosisName = DiagnosisName of string
+    and DiagnosisDate = DiagnosisDate of System.DateTime
+
     type Json =
         { [<JsonProperty("lab")>] Lab: LabJson
-          [<JsonProperty("report")>] Report: ReportJson }
+          [<JsonProperty("report")>] Report: ReportJson
+          [<JsonProperty("patient")>] Patient: PatientJson }
     and LabJson =
         { Name: string
           StreetAddress: string
@@ -17,6 +24,15 @@ module Tempus =
         { [<JsonProperty("reportId")>] ReportId: string
           [<JsonProperty("signing_pathologist")>] SigningPathologist: string
           [<JsonProperty("signout_date")>] SignoutDate: string }
+    and PatientJson =
+        { [<JsonProperty("firstName")>] FirstName: string
+          [<JsonProperty("lastName")>] LastName: string
+          [<JsonProperty("tempusId")>] TempusId: string
+          [<JsonProperty("emr_id")>] MRN: string
+          [<JsonProperty("sex")>] Sex: string
+          [<JsonProperty("DoB")>] DateOfBirth: string
+          [<JsonProperty("diagnosis")>] Diagnosis: string
+          [<JsonProperty("diagnosisDate")>] DiagnosisDate: string }
 
     module Json =
         let deserialize jsonText =
